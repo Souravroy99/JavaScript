44e477c6dad0846d08d7792a8a90d32f721e825d
Souravroy99 <souravroy200418@gmail.com>
2025-12-12 11:38:12 +0530

    Variable

---
+++ Hitesh Dec 12.12.25/01_variables.js
@@ -0,0 +1,7 @@
+const a = 1
+let b = 2
+var c = 3
+d = 4 // This will behave like global variable
+
+console.log(`${a}, ${b}, ${c}, ${d}`);
+console.log(`${typeof(a)}, ${typeof(b)}, ${typeof(c)}, ${typeof(d)}`);
