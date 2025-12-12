4794468e038f730b0d9242b25fcf2a0eb1f7a3f3
Souravroy99 <souravroy200418@gmail.com>
2025-12-12 13:28:55 +0530

    Basics complete

--- Hitesh Dec 12.12.25/02_datatypes.js
+++ Hitesh Dec 12.12.25/02_datatypes.js
@@ -24,3 +24,33 @@ console.log(id1 == id2); // false
 
 
 // Type of 'function' is "object function"
+
+
+// Stack memory allocates for all primitive datatypes
+
+/*
+✔ Reference (non-primitive) data types
+
+    The reference (pointer) is stored in the stack
+    The actual value (object/array/function) is stored in the heap
+*/
+
+
+
+/*
+// Range: 0 <= Math.random() <= 1
+
+    To generate a random number between x and y (inclusive) ==>
+*/
+
+let x = 10, y = 20 ;
+let val = x + Math.floor(Math.random() * (y - x + 1)) ; 
+// console.log(val);
+
+
+const myDate = new Date() ;
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+
+const myDateNow = Date.now();
+console.log(myDateNow);
