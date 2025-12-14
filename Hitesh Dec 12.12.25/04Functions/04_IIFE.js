918d837f57fefa3496e2547b8ba2816c4bf03288
Souravroy99 <souravroy200418@gmail.com>
2025-12-14 12:02:30 +0530

    IFFE

---
+++ Hitesh Dec 12.12.25/04Functions/04_IIFE.js
@@ -0,0 +1,44 @@
+(()=>{
+    console.log("Immediately Invoked Function Expression");
+})();
+
+
+/************************ Key characteristics ************************/
+
+// 1.) Runs immediately
+/*
+    (function () {
+        console.log("Hello");
+    })();
+*/
+
+
+// 2.) Creates its own scope: Variables inside an IIFE do not pollute the global scope
+/*
+    (function () {
+        let x = 10;
+    })();
+
+    console.log(x); // ❌ ReferenceError
+*/
+
+
+// 3.) Can return values
+/*
+    const result = (function () {
+        return 42;
+    })();
+
+    console.log(result); // 42
+*/
+
+
+// 4.) Can take parameters
+/*
+    (function (name) {
+        console.log("Hello", name);
+    })("Sourav");
+*/
+
+
+// 5.) IIFE prevents global namespace pollution by creating a private scope
