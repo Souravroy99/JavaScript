6f05d8405ee694c3aa79075b394510bf4238bcd0
Souravroy99 <souravroy200418@gmail.com>
2026-01-02 00:04:04 +0530

    Garbage collection

--- Interview_Qs/01_Closures.js
+++ Interview_Qs/01_Closures.js
@@ -1,16 +1,15 @@
 const funcA = () => {
     const ele = 10 
-
+    
     const funcB = () => {
         console.log(`Values: ${ele}`)
     }
-
+    
     return funcB
 }
 
-const funcC = funcA()
-
-funcC()
+// const funcC = funcA()
+// funcC()
 
 
 /*
@@ -20,4 +19,51 @@ Closure:
     even after that outer function has finished executing.
 
 In short: 👉 Function + its lexical scope = Closure
+*/
+
+
+
+// Advantages: 1.) Data hiding + Encapsulation, 2.) Cleaner and modular code    
+
+function A() {
+    var count = 10 
+
+    function increment() {
+        count++
+        console.log(`Count value: ${count}`)
+    }
+
+    return increment ;
+}
+
+const magic1 = A()
+magic1()
+magic1()
+magic1()
+
+console.log();
+
+const magic2 = A()
+magic2()
+magic2()
+magic2()
+
+
+
+
+
+/*
+
+❌ Disadvantages of Closures: 
+
+1.) Higher memory consumption: 
+    👉 Variables captured by closures are not garbage collected until the closure is destroyed.
+
+2.) Risk of memory leaks
+
+3.) Performance overhead:
+    👉 Accessing variables through scope chains can be slightly slower than local variables.
+
+Performance overhead
+
 */
---
+++ Interview_Qs/03_Garbage_Collection.js
@@ -0,0 +1,14 @@
+/*
+
+    Garbage Collection (GC) is an automatic memory management process in 
+    which the runtime system identifies and frees memory that is no longer 
+    being used by a program.
+
+*/
+
+
+
+/*
+    Garbage collection and closures are closely related because closures can keep 
+    variables alive in memory longer than expected.
+*/
